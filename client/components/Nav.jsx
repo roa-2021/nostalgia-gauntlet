558031e2b971f0358abaa8836d286da2797fd66f
Jordan Cooper <jordan@Jordans-MacBook-Air.local>
2021-08-27 11:31:15 +1200

    added styles to everything

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,11 +1,12 @@
-import React from "react"
+import React from 'react'
 import { Link } from 'react-router-dom'
 
 function Nav () {
   return (
     <>
       <ul>
-        <Link to={"/"}> Home</Link>
+        <Link to={'/'} className='btn btn-outline-warning btn-sm nav-button'> Home</Link>
+        <Link to={'/about-us'} className='btn btn-outline-warning btn-sm nav-button'> About Us</Link>
       </ul>
     </>
   )
