f1eba6f36f5a39d16170fff737f77ef552115de0
Jordan Cooper <jordan@Jordans-MacBook-Air.local>
2021-08-27 14:50:37 +1200

    coverted TotallySpies page to use something pressurrre!!

--- client/components/TotallySpies.jsx
+++ client/components/TotallySpies.jsx
@@ -1,11 +1,45 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React, { useState } from 'react'
 
-function TotallySpies() {
+function TotallySpies(props) {
+  const rightAnswer = () => {
+    rightClickHandler()
+    setTimeout(() => {
+      props.history.push('/Dragonball')
+    }, 1000)
+  }
+
+  const wrongAnswer = (target) => {
+    wrongClickHandler(target)
+    setTimeout(() => {
+      props.history.push('/')
+    }, 1000)
+  }
+
+  const [colour, setColour] = useState({
+    PowerPuff: 'btn btn-warning q-opt',
+    KimPoss: 'btn btn-warning q-opt',
+    TotallySpies: 'btn btn-warning q-opt'
+  })
+
+  const rightClickHandler = () => {
+    setColour({
+      PowerPuff: 'btn btn-danger q-opt',
+      KimPoss: 'btn btn-danger q-opt',
+      TotallySpies: 'btn btn-success q-opt'
+    })
+  }
+
+  const wrongClickHandler = (char) => {
+    setColour({
+      ...colour,
+      [char]: 'btn btn-danger q-opt'
+    })
+  }
+
   return (
     <>
     <div>
-        <h1>Challenge number 2</h1> 
+        <h1>Challenge number 2</h1>
         <h2>Name this show</h2>
     </div>
 
@@ -14,15 +48,16 @@ function TotallySpies() {
       </div>
 
       <div>
-        <Link to={"/Dragonball"}> Totally Spies</Link>
+      <button className={colour.PowerPuff} type='button' onClick={() => wrongAnswer('PowerPuff')}>PowerPuff Girls</button>
       </div>
 
       <div>
-        <Link to={"/"}> Kim Possible</Link>
+      <button className={colour.KimPoss} type='button' onClick={() => wrongAnswer('KimPoss')}>Kim Possible</button>
       </div>
 
       <div>
-        <Link to={"/"}> PowerPuff Girls</Link>
+      <button className={colour.TotallySpies} type='button' onClick={() => rightAnswer('TotallySpies')}>Totally Spies</button>
+
       </div>
     </>
 
