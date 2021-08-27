4cec33868ac21c5b7fa1b800ad5954f00ef7e66b
zoe-joblin <85871720+zoe-joblin@users.noreply.github.com>
2021-08-27 12:10:23 +1200

    add Pokemon file changes

--- client/components/Pokemon.jsx
+++ client/components/Pokemon.jsx
@@ -1,33 +1,49 @@
-import React from "react"
 import { Link } from "react-router-dom"
+import React, { useState } from 'react'
 
-function Pokemon() {
-
+function Pokemon (props) {
+  const [colour, setColour] = useState({
+    Evie: 'black;',
+    Jigglypuff: 'black;',
+    Meowth: 'black;'
+  })
+  const clickHandler = () => {
+    setColour({
+      ...colour,
+      Evie: 'green;',
+      Jigglypuff: 'red;',
+      Meowth: 'red;'
+    })
+  }
   return (
     <>
-    <div>
+      <div>
         <h1>Challenge number #</h1> 
         <h2>Who's that Pokemon?</h2>
-    </div>
+      </div>
 
       <div>
         <img src={'./images/pokemans.png'} />
       </div>
-
+   
       <div>
-        <Link to={"/TotallySpies"}> Evie</Link>
+        <Link to={'/TotallySpies'}> 
+          <h2 style={{ color: `${colour.Evie}` }} onClick={clickHandler}>Evie</h2>
+        </Link>
       </div>
 
       <div>
-        <Link to={"/"}> Incorrect Answer</Link>
+        <Link to={'/'}>
+          <h2 style={{ color: `${colour.Jigglypuff}` }} onClick={clickHandler}>Jigglypuff</h2>
+        </Link>
       </div>
 
       <div>
-        <Link to={"/"}> Incorrect Answer</Link>
+        <Link to={'/'}>
+          <h2 style={{ color: `${colour.Meowth}` }} onClick={clickHandler}>Meowth</h2>
+        </Link>
       </div>
     </>
-
-
   )
 }
 
