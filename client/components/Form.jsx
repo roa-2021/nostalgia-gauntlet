f1dadb3749e59d0249a574b0e34a93597c7b51a7
phill-mutu <7622261+Lysias91@users.noreply.github.com>
2021-08-27 12:34:32 +1200

    fix form

--- client/components/Form.jsx
+++ client/components/Form.jsx
@@ -2,23 +2,27 @@ import React, { useState, useEffect } from "react"
 
 function Form () {
     const [name, setName] = useState('')
+    const [selectedName, setSelectedName] = useState('')
+    const challengerName = setSelectedName(name)
 
     const handleSubmit = (evt) => {
       evt.preventDefault()
-      // alert(`Hello ${name}!`)
+      
       setName('')
     }
-  
+    
     const handleChange = (evt) => {
       setName(evt.target.value)
+      
     }
-  
+    
     return (
       <>
         <form onSubmit={handleSubmit}>
           <label htmlFor="name" >What is your name?</label>
           <input name="What is your name" placeholder='Challenger Name' value={name} type="text" onChange={handleChange} />
           <button className='btn btn-warning btn-sm'>Submit</button>
+            {selectedName}
         </form>
       </>
     )
